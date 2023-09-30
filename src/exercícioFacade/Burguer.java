86e1b4c2bcabccb5213623d487cb45b4bcdbc6df
Erica Marcondes <erica.elen@live.com>
2023-09-30 20:39:49 -0300

    exercicio facade 27/09/2023

---
+++ src/exercícioFacade/Burguer.java
@@ -0,0 +1,14 @@
+package exercícioFacade;
+
+public class Burguer extends Produto {
+
+    int gramas;
+
+    public int getGramas() {
+        return gramas;
+    }
+
+    public void setGramas(int gramas) {
+        this.gramas = gramas;
+    }
+}
---
+++ src/exercícioFacade/Produto.java
@@ -0,0 +1,23 @@
+package exercícioFacade;
+
+public class Produto {
+
+    String descricao;
+    double preco;
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
